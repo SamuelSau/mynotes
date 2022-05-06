db947ad790d3e05da392c3f136e79be45b44dde1
Samuel Sau <samuel51@usf.edu>
2022-05-05 22:55:07 -0400

    modified all the dart files and added notes_view_states and implemented logic for logging out user

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
 import 'firebase_options.dart';
 import 'views/login_view.dart';
+import 'dart:developer' as devtools show log;
 void main() {
    WidgetsFlutterBinding.ensureInitialized();
    runApp(MaterialApp(
@@ -40,16 +41,15 @@ class HomePage extends StatelessWidget {
                final user = FirebaseAuth.instance.currentUser;
                if (user!=null){
                     if(user.emailVerified){
-                      print('Email is verified');
+                      return const NotesView();
                     }
-                    else{
+                    else {
                       return const VerifyEmailView();
-                }
+                    }
                }
-               else{
+               else {
                     return const LoginView();
                }
-               return const Text('Done');
             default:
               return const CircularProgressIndicator();
           }
@@ -57,3 +57,65 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({ Key? key }) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch(value){
+                case MenuAction.logout:
+                  final shouldLogOut = await showLogOutDialog(context);
+                  if (shouldLogOut){
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil('/login/', (_) => false,);
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return const [ 
+                PopupMenuItem<MenuAction>( 
+                   value: MenuAction.logout, 
+                   child: Text('Log out'),
+                ),
+              ];
+            },
+          )
+        ]
+      ),
+         body: const Text('Hello World'),
+    );
+  }
+}
+
+Future<bool>showLogOutDialog(BuildContext context){
+  return showDialog<bool>(context: context, 
+  builder: (context){
+    return AlertDialog(
+      title: const Text('Sign out'),
+      content: const Text('Are you want to sign out?'),
+      actions: [
+        TextButton(onPressed:() {
+          Navigator.of(context).pop(false);
+        }, child: const Text('Cancel')),
+        TextButton(onPressed:() {
+          Navigator.of(context).pop(true);
+        }, child: const Text('Cancel')),
+        ],
+       );
+      },
+    ).then((value) => value ?? false); //this condition checks if the user gets the dialouge box, it's not guareented a response this they can exit or not respond to it, hence it can be optional (null)
+   }
